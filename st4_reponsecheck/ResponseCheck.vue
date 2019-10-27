3c101a117b8a0a20595008d49a6285e1b397aa79
devksh930 <devksh930@icloud.com>
2019-10-27 20:19:39 +0900

    response game finish

--- st4_reponsecheck/ResponseCheck.vue
+++ st4_reponsecheck/ResponseCheck.vue
@@ -2,13 +2,16 @@
     <div>
     <div id="screen" :class="state" @click="onClickScreen">{{message}}</div> <!-- v bind:class alias :class -->
     <div>
-        <div>평균 시간 : {{}}</div>
+        <div>평균 시간 : {{result.reduce((a, c)=>a+c,0)/result.length || 0}}ms</div>
         <button @click="onReset">리셋 버튼</button>
     </div>
     </div>
 </template>
 
 <script>
+    let startTime = 0;
+    let endTime = 0;
+    let timeOut = null;
     export default {
         data() {
         return{
@@ -19,18 +22,29 @@
         },
         methods: {
             onReset(){
-
+                this.result = [];
             },
             onClickScreen(){
             if(this.state === 'waiting') {
                 this.state = 'ready';
+                this.message = '초록색이 되면 클릭하세요.';
+                timeOut = setTimeout(() => {
+                    this.state = 'now';
+                    this.message = '지금 클릭!';
+                    startTime = new Date(); //data에 안넣는 이유는 화면과 관련이 없다.
+                }, Math.floor(Math.random()*1000)+2000); //2-3초
             }else if(this.state === 'ready'){
-                this.state = 'now';
-            }else if(this.state === 'now'){
+                clearTimeout(timeOut);
                 this.state = 'waiting';
+                this.message = '너무 성급하시군요! 초록색이 된후 클릭하세요';
+            }else if(this.state === 'now'){
+                endTime = new Date();
+                this.state = 'waiting';
+                this.message = '클릭해서 시작하세요. ';
+                this.result.push(endTime - startTime);
             }
-            },
-        }
+            }
+        },
     };
 </script>
 
