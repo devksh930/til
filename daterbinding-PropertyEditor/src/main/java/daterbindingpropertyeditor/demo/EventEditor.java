821522ddbf9472bcd10e7ff8c6cab54534d3f011
devksh930 <devksh930@icloud.com>
2020-07-12 22:29:20 +0900

    spring Databinding

--- daterbinding-PropertyEditor/src/main/java/daterbindingpropertyeditor/demo/EventEditor.java
+++ daterbinding-PropertyEditor/src/main/java/daterbindingpropertyeditor/demo/EventEditor.java
@@ -1,4 +1,18 @@
 package daterbindingpropertyeditor.demo;
 
-public class EventEditor {
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorSupport;
+
+public class EventEditor extends PropertyEditorSupport {
+    @Override
+    public String getAsText() {
+//        getValue(); propertyEditor가 받은 객체를 getValue로 가져올수 있다
+        Event event = (Event) getValue();
+        return event.getId().toString();
+    }
+
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        setValue(new Event(Integer.parseInt(text)));
+    }
 }
