0ea31e295120fa4858efa6830e3b3456e896427e
devksh930 <devksh930@icloud.com>
2020-08-07 07:34:11 +0900

    spring boot redis

---
+++ spring-redis/src/main/java/me/devksh930/RedisRunner.java
@@ -0,0 +1,38 @@
+package me.devksh930;
+
+import me.devksh930.account.Account;
+import me.devksh930.account.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class RedisRunner implements ApplicationRunner {
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set("sungho", "devksh930");
+        values.set("springboot", "2.0");
+        values.set("hello", "world");
+
+        Account account = new Account();
+        account.setEmail("devksh930@icloud.com");
+        account.setUsername("sungho");
+
+        accountRepository.save(account);
+        Optional<Account> byId = accountRepository.findById(account.getId());
+        System.out.println(byId.get().getUsername());
+        System.out.println(byId.get().getEmail());
+    }
+}
