39656dabe011b2ec32f0e6b31141059096408af6
devksh930 <devksh930@icloud.com>
2020-08-17 14:00:22 +0900

    DispatcherServlet

---
+++ ServletListener-ServletFilter/src/main/java/me/devksh930/AppConfig.java
@@ -0,0 +1,9 @@
+package me.devksh930;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AppConfig {
+}
---
+++ ServletListener-ServletFilter/src/main/java/me/devksh930/HelloService.java
@@ -0,0 +1,11 @@
+package me.devksh930;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String getName() {
+        return "sungho";
+    }
+}
