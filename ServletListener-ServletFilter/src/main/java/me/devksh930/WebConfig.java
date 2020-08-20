df909dbb0cc7e02ff500c0df7f3d172abe37a458
devksh930 <devksh930@icloud.com>
2020-08-21 00:18:19 +0900

    SPRING MVC

--- ServletListener-ServletFilter/src/main/java/me/devksh930/WebConfig.java
+++ ServletListener-ServletFilter/src/main/java/me/devksh930/WebConfig.java
@@ -2,8 +2,9 @@ package me.devksh930;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
 @Configuration
 @ComponentScan
-public class AppConfig {
+public class WebConfig {
 }
