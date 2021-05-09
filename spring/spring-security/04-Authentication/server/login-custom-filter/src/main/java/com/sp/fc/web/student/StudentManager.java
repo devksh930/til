5f77aa65c08410291eb2cbeecc7da44fe0a184bc
devksh930 <devksh930@icloud.com>
2021-05-10 05:28:50 +0900

    TIL : Spring 시큐리티 MultiChainProxy

---
+++ spring/spring-security/04-Authentication/server/login-custom-filter/src/main/java/com/sp/fc/web/student/StudentManager.java
@@ -0,0 +1,46 @@
+package com.sp.fc.web.student;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Set;
+
+@Component
+public class StudentManager implements AuthenticationProvider, InitializingBean {
+
+    private HashMap<String, Student> studentDB = new HashMap<>();
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        StudentAuthenticationToken token = (StudentAuthenticationToken) authentication;
+        if (studentDB.containsKey(token.getCredentials())) {
+            Student student = studentDB.get(token.getCredentials());
+            return StudentAuthenticationToken.builder()
+                    .principal(student)
+                    .details(student.getUsername())
+                    .authenticated(true)
+                    .build();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication == StudentAuthenticationToken.class;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Set.of(
+                new Student("stu1", "김성호", Set.of(new SimpleGrantedAuthority("ROLE_STUDENT"))),
+                new Student("stu2", "홍길동", Set.of(new SimpleGrantedAuthority("ROLE_STUDENT"))),
+                new Student("stu3", "강아지", Set.of(new SimpleGrantedAuthority("ROLE_STUDENT")))
+        ).forEach(s -> studentDB.put(s.getId(), s));
+    }
+}
